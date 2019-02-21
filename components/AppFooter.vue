abeadaf804007f000216e2995582d2bff92af95e
Chris <info@steampixel.de>
2019-02-21 22:26:19 +0100

    Added<matomo

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -50,7 +50,25 @@
   import HalfHeight from '~/components/HalfHeight.vue'
 
   export default {
-    components: { CookieLaw, HalfHeight }
+    components: { CookieLaw, HalfHeight },
+    mounted () {
+
+      // Matomo
+      var _paq = _paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+      _paq.push(["setCookieDomain", "*.steampixel.de"]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//matomo.steampixel.de/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+
+    }
   }
 </script>
 
